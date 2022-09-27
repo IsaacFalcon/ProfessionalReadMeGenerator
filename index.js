0fb37726ad660f8c1fc6b03c78cd1ab87b3fecf7
IsaacFalcon <ice6490@gmail.com>
2022-09-27 17:28:08 -0500

    added gitignore files, added questions to index.js, added proper readme file to be generated on generateMarkdown file

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a Description of the Project'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please provide Installation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should someone contribute to this project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which License would you like to use?',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email'
+    },
+]).then((answers) => fs.writeFileSync('./utils/READMESample.md', generateMarkdown(answers)))
+  .then(() => console.log('File was created'))
+  .catch((err) => console.log(err));
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Did Not wind up using these functions
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
---
+++ utils/READMESample.md
@@ -0,0 +1,31 @@
+# Test Title 
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+
+  ## Description:
+  Descript
+
+  ## Installation:
+  Install
+
+  ## Usage:
+  Usage
+
+  ## Contribution:
+  Contirbute
+
+  ## Testing:
+  test
+
+  ## License:
+  - License: MIT
+  - [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Additional Info:
+  - Github: [github](https://github.com/github)
+  - Email: @gmail.com
